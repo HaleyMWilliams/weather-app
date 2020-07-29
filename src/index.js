ca8d534f99e441a870b46489d708e33fe404e9d0
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2020-07-29 11:26:28 -0400

    initial-upload
    All app code to date

---
+++ src/index.js
@@ -0,0 +1,158 @@
+function showWeather(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+
+  document.querySelector("#temp").innerHTML = response.data.main.temp;
+
+  document.querySelector("#feels-like").innerHTML =
+    response.data.main.feels_like;
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+
+  document.querySelector(".weatherDescription").innerHTML =
+    response.data.weather[0].description;
+}
+
+function searchCity(city) {
+  let apiKey = "f965be3a8c73441341db743d519d1c93";
+  let units = "&units=imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "f965be3a8c73441341db743d519d1c93";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function showDateTime(event) {
+  let now = new Date();
+  let time = document.querySelector("#time");
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let hours = [
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+  ];
+
+  let minutes = [
+    "00",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+    "31",
+    "32",
+    "33",
+    "34",
+    "35",
+    "36",
+    "37",
+    "38",
+    "39",
+    "40",
+    "41",
+    "42",
+    "43",
+    "44",
+    "45",
+    "46",
+    "47",
+    "48",
+    "49",
+    "50",
+    "51",
+    "52",
+    "53",
+    "54",
+    "55",
+    "56",
+    "57",
+    "58",
+    "59",
+    "60",
+  ];
+
+  let day = days[now.getDay()];
+  let hour = hours[now.getHours()];
+  let minute = minutes[now.getMinutes()];
+  time.innerHTML = `${day} ${hour}:${minute}`;
+}
+
+let newCityForm = document.querySelector("#search-form");
+newCityForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+showDateTime();
